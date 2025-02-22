c2df46171ef9326d25d8e2caa0c8fc2618a7661f
Aditya Sharma <adisharma4857@gmail.com>
2025-02-22 18:55:35 +0530

    improved header logo and button sizes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,14 +9,14 @@ export default function Header () {
         <header className="sticky top-0 border-b border-b-secondary py-5 px-4 md:px-12 bg-background/90 backdrop-blur">
             <div className="w-full max-w-7xl mx-auto flex justify-between items-center">
                 <Link href="/">
-                <Image src={Logo} alt="RogiSetu Header Logo Image" className="max-w-40" />
+                <Image src={Logo} alt="RogiSetu Header Logo Image" className="max-w-32 md:max-w-40" />
                 </Link>
                 <NavLinks />
                 <div className="flex gap-2">
-                    <Button variant="outline" asChild>
+                    <Button variant="outline" className="px-3 md:px-4 h-8 md:h-9 text-xs md:text-sm" asChild>
                         <Link href="/login">Login</Link>
                     </Button>
-                    <Button asChild>
+                    <Button className="px-3 md:px-4 h-8 md:h-9 text-xs md:text-sm" asChild>
                         <Link href="/register">Register</Link>
                     </Button>
                 </div>
