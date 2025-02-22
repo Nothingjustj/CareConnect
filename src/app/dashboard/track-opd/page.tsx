7c8838eaf80abedf4238613a413245afac9d427d
Aditya Sharma <adisharma4857@gmail.com>
2025-02-22 19:26:47 +0530

    fixed dashboard layout

--- src/app/dashboard/track-opd/page.tsx
+++ src/app/dashboard/track-opd/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 export default function TrackOpd() {
     return (
-        <div className="px-2 mx-auto w-full max-w-4xl">
+        <div className="px-2 py-6 mx-auto w-full max-w-4xl">
             <h1 className="font-bold text-2xl md:text-3xl">Track Your OPD Status</h1>
             <div className="flex items-center gap-3 mt-4">
                 <Input placeholder="Enter your token no. (eg. A-123)" />
