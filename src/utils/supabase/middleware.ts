c8cf9ff3f972652c5745186d93989b0c26dc8a1e
Nothingjustj <jagritisharma.it27@gmail.com>
2025-10-15 10:18:16 +0530

    Fixed middleware cookie set issue for Vercel deployment

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -18,7 +18,8 @@ export async function updateSession(request: NextRequest) {
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set(name, value, options)
+            request.cookies.set(name, value)
+
           )
           supabaseResponse = NextResponse.next({
             request,
